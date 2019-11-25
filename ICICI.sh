12bcdb273383e63d0d59aa42d227a00703eb24ea
whitetigerz <50932142+whitetigerz@users.noreply.github.com>
2019-11-25 02:20:51 -0800

    Create ICICI.sh

---
+++ ICICI.sh
@@ -0,0 +1,20 @@
+apt update
+apt upgrade
+apt install figlet
+apt install php
+apt install python
+apt install python2
+apt install git
+apt install ruby
+apt install nano
+gem install lolcat
+pkg install nodejs
+npm install -g bash-obfuscate
+pkg install unstable-repo
+echo -e "\033[1m \e[32 ICII BANK BY PASSING" |lolcat|lolcat
+echo -e "\033[1m \e[32 "ACCOUNT NO 001105010211 |lolcat|lolcat
+echo -e "\033[1m \e[32 ACCOUNT TYPE CURRENT" |lolcat|lolcat
+echo -e "\033[1m \e[32 IFSC CODE  ICIC0000011" |lolcat|lolcat
+echo -e "\033[1m \e[32 CHECKING CURRENT BALANCE ......PLEASE WAIT" |lolcat|lolcat
+echo -e "\033[1m \e[32 6.5 CR.R ...please wait" |lolcat|lolcat
+figlet Process Ended |lolcat|lolcat
