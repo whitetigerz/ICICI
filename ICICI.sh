1fc0fdad2b3d8aced0b01dcc42cd8f23f54d9555
whitetigerz <50932142+whitetigerz@users.noreply.github.com>
2019-11-25 03:15:47 -0800

    Update ICICI.sh

--- ICICI.sh
+++ ICICI.sh
@@ -15,6 +15,11 @@ echo -e "\033[1m \e[32 ICII BANK BY PASSING" |lolcat|lolcat
 echo -e "\033[1m \e[32 "ACCOUNT NO 001105010211 |lolcat|lolcat
 echo -e "\033[1m \e[32 ACCOUNT TYPE CURRENT" |lolcat|lolcat
 echo -e "\033[1m \e[32 IFSC CODE  ICIC0000011" |lolcat|lolcat
-echo -e "\033[1m \e[32 CHECKING CURRENT BALANCE ......PLEASE WAIT" |lolcat|lolcat
+echo -e "\033[1m \e[32 Dumping Amount ......PLEASE WAIT" |lolcat|lolcat
 echo -e "\033[1m \e[32 6.5 CR.R ...please wait" |lolcat|lolcat
-figlet Process Ended |lolcat|lolcat
+echo -e "\033[1m \e[32 3.25 CR.R ...please wait" |lolcat|lolcat
+echo -e "\033[1m \e[32 CBD Account Details" |lolcat|lolcat
+echo -e "\033[1m \e[32 Account NO 1002332540" |lolcat|lolcat
+echo -e "\033[1m \e[32 IBAN NO AE230230000001002332540" |lolcat|lolcat
+echo -e "\033[1m \e[32 " |lolcat|lolcat
+figlet Transfer Started |lolcat|lolcat
